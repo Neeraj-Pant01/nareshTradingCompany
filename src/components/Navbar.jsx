a65e3bd7ff08c46f193f26c746e0ac8b6cc0c2bd
Neeraj-Pant01 <npant923@gmail.com>
2024-06-08 23:07:44 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex sticky top-0 py-2 px-4 bg-[#7CB9E8]'>
+        <div className='flex flex-col'>
+            <h1 className='text-[white] text-2xl font-bold'>Naresh Trading</h1>
+            <span className='text-[black] font-bold'>Company</span>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
