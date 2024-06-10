9ad2444918bc6c0a60a26b054445f35f58e0e80c
Neeraj-Pant01 <npant923@gmail.com>
2024-06-10 15:01:38 +0530

    updated 3.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='flex sticky items-center justify-between top-0 md:py-4 py-2 px-4 z-50 bg-[#7CB9E8]'>
         <div className='flex flex-col'>
-            <h1 className='text-[white] text-xl font-bold uppercase'>NareshTrading</h1>
+            <h1 className='text-[white] text-2xl font-bold uppercase'>Naresh Trading</h1>
             <span className='text-[white] font-semibold uppercase'>Company</span>
         </div>
         <div className='flex text-[white] uppercase underline font-semibold'>
